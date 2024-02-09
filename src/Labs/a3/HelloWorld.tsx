ec34ae18fbb8e07f7152231d317db1e6ef95f66c
bharatchawla53 <bharatchawla53@gmail.com>
2024-02-09 12:36:31 -0500

    lab section 2.2

---
+++ src/Labs/a3/HelloWorld.tsx
@@ -0,0 +1,9 @@
+function HelloWorld() {
+    return (
+        <div className="container">
+            <h1>Hello World!</h1>
+        </div>
+    );
+}
+
+export default HelloWorld;
---
+++ src/Labs/a3/JavaScript/arrays/WorkingWithArrays.tsx
@@ -0,0 +1,40 @@
+import AddingAndRemovingDataToFromArrays from "./AddingAndRemovingDataToFromArrays ";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import FilterFunction from "./FilterFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+
+function WorkingWithArrays() {
+    var functionScoped = 2;
+    let blockScoped = 5;
+    const constant1 = functionScoped - blockScoped;
+    let numberArray1 = [1, 2, 3, 4, 5];
+    let stringArray1 = ['string1', 'string2'];
+
+    let variableArray1 = [
+        functionScoped, blockScoped,
+        constant1, numberArray1, stringArray1
+    ];
+
+
+    return (
+        <>
+            <h2>Working with Arrays</h2>
+            numberArray1 = {numberArray1}<br />
+            stringArray1 = {stringArray1}<br />
+            variableArray1 = {variableArray1}
+
+            <ArrayIndexAndLength />
+            <AddingAndRemovingDataToFromArrays />
+            <ForLoops />
+            <MapFunction />
+            <FindFunction />
+            <FindIndex />
+            <FilterFunction />
+        </>
+    );
+}
+
+export default WorkingWithArrays
