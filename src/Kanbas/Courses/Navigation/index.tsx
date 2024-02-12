532cd7385903e03551481762059358c60713c208
bharatchawla53 <bharatchawla53@gmail.com>
2024-02-12 15:36:30 -0500

    section 3.4

---
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -0,0 +1,29 @@
+import { Link, useLocation } from "react-router-dom";
+import "./index.css";
+
+function CourseNavigation() {
+    const links = ["Home", "Modules", "Piazza", "Zoom Meetings", "Assignments", "Quizzes",
+        "Grades", "People", "Panopto Video", "Discussions", "Announcements", "Pages", "Files",
+        "Rubrics", "Outcomes", "Colloborations", "Syllabus", "Settings"];
+
+    const { pathname } = useLocation();
+
+    return (
+        <div id="course-nav-sidebar">
+
+            <span id="section-tabs-header-subtitle" className="ellipsis">
+                202430_2 Spring 2024 Semester Full Term Grad
+            </span>
+
+            <ul>
+                {links.map((link, index) => (
+                    <li key={index} className={pathname.includes(link) ? "active" : ""} >
+                        <Link to={link}>{link}</Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default CourseNavigation
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,30 +1,68 @@
 import { useParams } from "react-router";
 import { courses } from "../Database";
 import { HiMiniBars3 } from "react-icons/hi2";
-import { Link } from "react-router-dom";
+import { Link, useLocation, Navigate, Route, Routes } from "react-router-dom";
+import CourseNavigation from "./Navigation";
 import "./index.css"
+
 
 function Courses() {
 
     const { courseId } = useParams();
     const course = courses.find((course) => course._id === courseId);
 
+    const { pathname } = useLocation();
+    const pathParts = decodeURIComponent(pathname).split("/");
+    const activeBreadcrumb = pathParts[pathParts.length - 1];
+
     // TODO make breadcrumb responsive refer to the old code
     return (
         <div className="course-main">
             <div className="d-flex align-items-center">
                 <HiMiniBars3 size={'2em'} />
-                <nav>
+                <nav className="custom-breadcrumb" aria-label="breadcrumb">
                     <ol className="breadcrumb m-0 ms-3">
                         <li className="breadcrumb-item">
                             <Link to={`/Kanbas/Courses/${course?._id}/Home`}>
                                 {course?.number}{course?.startDate}
                             </Link>
                         </li>
+                        <li className="breadcrumb-item active" aria-current="page">
+                            {activeBreadcrumb}
+                        </li>
                     </ol>
                 </nav>
             </div>
             <hr />
+
+            <CourseNavigation />
+
+            <div className="course-routes">
+                <Routes>
+                    <Route path="/" element={<Navigate to="Home" />} />
+                    <Route path="Home" element={<h1>Home</h1>} />
+                    <Route path="Modules" element={<h1>Modules</h1>} />
+                    <Route path="Piazza" element={<h1>Piazza</h1>} />
+                    <Route path="Zoom Meetings" element={<h1>Zoom Meetings</h1>} />
+                    <Route path="Assignments" element={<h1>Assignments</h1>} />
+                    <Route path="Quizzes" element={<h1>Quizzes</h1>} />
+                    <Route path="Assignments/:assignmentId" element={<h1>Assignment Editor</h1>} />
+                    <Route path="Grades" element={<h1>Grades</h1>} />
+                    <Route path="People" element={<h1>People</h1>} />
+                    <Route path="Panopto Video" element={<h1>Panopto Video</h1>} />
+                    <Route path="Discussions" element={<h1>Discussions</h1>} />
+                    <Route path="Announcements" element={<h1>Announcements</h1>} />
+                    <Route path="Pages" element={<h1>Pages</h1>} />
+                    <Route path="Files" element={<h1>Files</h1>} />
+                    <Route path="Rubrics" element={<h1>Rubrics</h1>} />
+                    <Route path="Outcomes" element={<h1>Outcomes</h1>} />
+                    <Route path="Colloborations" element={<h1>Colloborations</h1>} />
+                    <Route path="Syllabus" element={<h1>Syllabus</h1>} />
+                    <Route path="Settings" element={<h1>Settings</h1>} />
+                </Routes>
+            </div>
+
+
         </div>
     );
 }
