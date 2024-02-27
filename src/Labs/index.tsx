a5d343ee69b924fb44cabed0372b6b0afb2c910d
bharatchawla53 <bharatchawla53@gmail.com>
2024-02-27 12:40:51 -0500

    section 2.1, 2.2, 2.3

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,11 +1,22 @@
 import Assignment3 from "./a3";
 import Nav from "../Nav";
+import { Routes, Route, Navigate } from "react-router";
+import Assignment4 from "./a4";
 
 function Labs() {
     return (
         <div>
             <Nav />
-            <Assignment3 />
+            <Routes>
+                <Route path="/"
+                    element={<Navigate
+                        to="a3" />} />
+                <Route path="a3"
+                    element={<Assignment3 />} />
+                <Route path="a4"
+                    element={<Assignment4 />} />
+            </Routes>
+
         </div>
     );
 }
